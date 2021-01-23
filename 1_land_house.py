62f419e06032747124ef3fc34b90fd7b919908d6
Allma <allma9019@gmail.com>
2021-01-23 23:52:15 +0900

    filter DDDGG

---
+++ 1_land_house.py
@@ -0,0 +1,35 @@
+# 다가구 주택 검색: 부동산 홈 -> 매물 -> 빌라·주택 -> 단독/다가구(True, 나머지는 False)
+# 지역: ex) 서울시 > 마포구 > 공덕동 
+# 거래방식: 매매
+# 가격대: 4억원 ~ 14억원
+# 기타옵션
+
+import requests
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# 다가구 주택 검색
+url = "https://land.naver.com"
+browser = webdriver.Chrome()
+browser.get(url)
+browser.maximize_window()
+
+mapOpen_elem = browser.find_element_by_xpath('//*[@id="lnb"]/div/ul/li[2]')
+mapOpen_elem.click()
+
+landHouse_elem = browser.find_element_by_xpath('//*[@id="wrap"]/div[1]/a[2]')
+landHouse_elem.click()
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+landHouse_types = soup.find_all("button", attrs = {"class":"filter_type"})
+landHouse_types_elem = browser.find_elements_by_class_name("filter_type")
+
+for i in range(len(landHouse_types)):
+    if landHouse_types[i]["value"] == "DDDGG":
+        continue
+    elif landHouse_types[i]["aria-pressed"] == "false":
+        continue
+    else:
+        landHouse_types_elem[i].click()
+
